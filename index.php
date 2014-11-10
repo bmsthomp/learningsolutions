f9abdcf06f4fc7ac0c527c58696b3677d8d92393
Brian Thompson <comet1101@gmail.com>
2014-11-10 09:03:10 -0600

    init file

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+</head>
+
+<body>
+<?php
+
+	$servername = "localhost";
+	$username = "thomp362_ls";
+	$password = "fall2014";
+	$dbname = "thomp362_learningsolutions";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
+	<?php 
+
+		$query = "select * from users;";
+		$x= $conn->query($query);
+		echo $x;
+	?>
+
+</body>
+</html>
