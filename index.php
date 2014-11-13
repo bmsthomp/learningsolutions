992243b2a8b34f603c7088bb6500271f07ca4629
Brian Thompson <comet1101@gmail.com>
2014-11-12 19:19:44 -0600

    File structure + bootstrap
    this will be the template for the master. branch from here.

--- index.php
+++ index.php
@@ -1,34 +1,44 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Learning Solutions Inc.</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+
 </head>
 
 <body>
+<!-- DB set up --> 
 <?php
 
+	// connection vars
 	$servername = "localhost";
 	$username = "thomp362_ls";
 	$password = "fall2014";
 	$dbname = "thomp362_learningsolutions";
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+	// Check connection
+	if ($conn->connect_error) {
+   		die("Connection failed: " . $conn->connect_error);
+	} 
 ?>
 
-	<?php 
+<!-- Begin site -->
+<h1>Learning Solutions Inc.</h1>
 
-		$query = "select * from users;";
-		$x= $conn->query($query);
-		
-		echo $x;
-		// this is a comment 
-		//this is another comment 
-	?>
 
+
+
+
+
+<!-- javascript libs -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
