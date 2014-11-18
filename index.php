c9036f199076b545f1df7697567818b39a67e3c2
Brian Thompson <comet1101@gmail.com>
2014-11-18 15:50:30 -0600

    form editting

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<?php session_start(); ?>
-<?php require 'head.php'; ?>
+<?php 
+	session_start();
+	require 'head.php'; ?>
 
 <!-- Begin site -->
 <h1>Learning Solutions Inc.</h1>
 
 <form method="POST" action="login.php">
 	<label>
-	<input name="username" required length="50" autofocus type="text" placeholder="Enter Email"><br>
-	<input name="password" required length="50" type="password" placeholder="Enter Password">
-	<input type="submit">
+	<input name="username" required length="50" autofocus type="email" placeholder="Enter Email"><br>
+	<input name="password" required length="50" type="password" placeholder="Enter Password"><br>
+	<input type="submit" value="Proceed">
 	<em>Not a member? <a href="signup.php">Sign up</a></em>
 </form>
+<br>
 
 <?php 
   
