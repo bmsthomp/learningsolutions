264a84a13cb6551da786c30df4f72a098b34c999
Brian Thompson <comet1101@gmail.com>
2014-11-18 09:42:16 -0600

    signup.php
    added fields to signup page

--- signup.php
+++ signup.php
@@ -2,11 +2,14 @@
 	session_start(); 
 	require 'head.php'; ?>
 
-
+<h1>Enter your information</h1>
 <form method='POST' action="signup.php">
-	<input name='fname' length='125' type='text' required>
-	<input name='lname' length='125' type='text' required>
-	<input name='email' length='125' type='email' required>
+	<input name='fname' length='125' type='text' required placeholder="First Name"><br>
+	<input name='lname' length='125' type='text' required placeholder="Last Name"><br>
+	<input name='email' length='125' type='email' required placeholder="Email"><br>
+	<input name='address' length='125' type='text' required placeholder="Shipping Address"><br>
+	<input name='baddress' length='125' type='text' required placeholder="Billing Address"><br>
+	<input name='password' length='125' type='password' required placeholder="Password">
 </form>
 
 <?php require 'footer.php'; ?>
